51016336150bb9837bcba03d3777e1793e839874
ruzhickiisasha <ruzhickiisasha@gmail.com>
2018-06-15 01:44:11 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 9999;
+
+module.exports = {
+    // Webpack configuration goes here
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            // First Rule
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            // Second Rule
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            camelCase: true,
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            favicon: 'public/favicon.ico'
+        })
+    ],
+    devServer: {
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true,
+        hot: true
+    }
+};
